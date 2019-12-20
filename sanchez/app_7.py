46fdf2860c0108a2684b67eb8b8b1b63bef70d8e
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-20 08:37:40 -0500

    se agrego el python file app_7.py en funcion de la libreria.py

---
+++ sanchez/app_7.py
@@ -0,0 +1,10 @@
+import libreria
+import os
+
+# 7 funcion promedio de notas
+
+x=float(os.sys.argv[1])
+y=float(os.sys.argv[2])
+z=float(os.sys.argv[3])
+t=libreria.promedio(x,y,z)
+print("El promedio es: ", t)
