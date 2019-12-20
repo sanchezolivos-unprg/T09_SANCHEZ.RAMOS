cb9e98f152819a902f8298bf2487d675075d92f3
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-20 07:58:09 -0500

    se agrego la segunda funcion a python file libreria.py al directory sanchez

--- sanchez/libreria.py
+++ sanchez/libreria.py
@@ -3,3 +3,9 @@
 def cal_vista (medicion_lentes,cantidad_lentes):
     resultado=((cantidad_lentes*medicion_lentes)/2)
     return resultado
+
+# funcion 2: calculo de la fuerza de la inercia
+
+def fuerza (masa_kg,aceleracion):
+    resultado=(masa_kg*aceleracion)
+    return resultado
