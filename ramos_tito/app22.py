fcafe352152ccdaf12b6eb1576bbde968d9533bb
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-20 09:50:42 -0500

    se agrego el python file app22.py de mi compañero Ramos Tito al directory ramos_tito

---
+++ ramos_tito/app22.py
@@ -0,0 +1,8 @@
+import libreria
+import os
+
+#ejercicio22 llamar funcion: hallar_aceleracion
+masa=int(os.sys.argv[1])
+fuerza=int(os.sys.argv[2])
+j=libreria.hallar_aceleracion(masa,fuerza)
+print(j)
