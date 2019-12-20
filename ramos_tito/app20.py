459e03497641a839ee7dda51717f3622cd367219
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-20 09:49:02 -0500

    se agrego el python file app20.py de mi compañero Ramos Tito al directory ramos_tito

---
+++ ramos_tito/app20.py
@@ -0,0 +1,10 @@
+import libreria
+import os
+
+#ejercicio 20 llamar funcion :area_trapecio
+
+bm=int(os.sys.argv[1])
+bmayor=int(os.sys.argv[2])
+alt=int(os.sys.argv[3])
+q=libreria.area_trapecio(bm,bmayor,alt)
+print(q)
