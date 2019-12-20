fa54fd6e7149bec480197c9b3f5a8aeec4d39370
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-20 09:38:34 -0500

    se agrego el python file app9.py de mi compañero ramos_tito al directory ramos_tito

---
+++ ramos_tito/app9.py
@@ -0,0 +1,9 @@
+import libreria
+import os
+#ejercicio9 llamr funcion : sumar_numero
+num1=int(os.sys.argv[1])
+num2=int(os.sys.argv[2])
+num3=int(os.sys.argv[3])
+msg=""
+s=libreria.suma_numeros(num1,num2,num3,msg)
+print(s)
