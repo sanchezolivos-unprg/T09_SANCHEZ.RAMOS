e9533392de924f0e1451b6ea844554d930093c14
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-20 08:54:54 -0500

    se agrego el python file app_18.py en funcion de la libreria.py

---
+++ sanchez/app_18.py
@@ -0,0 +1,11 @@
+import libreria
+import os
+
+# 18 funcion operacion matematica
+
+a=float(os.sys.argv[1])
+b=float(os.sys.argv[2])
+c=float(os.sys.argv[3])
+d=float(os.sys.argv[4])
+g=libreria.operacion(a,b,c,d)
+print("El resultado de la operacion es: ", g)
