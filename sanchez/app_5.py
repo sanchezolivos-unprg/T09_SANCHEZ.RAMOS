f1c8bc52a4a65677b0e12b8b2038216558ea8c44
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-20 08:35:46 -0500

    se agrego el python file app_5.py en funcion de la libreria.py

---
+++ sanchez/app_5.py
@@ -0,0 +1,9 @@
+import libreria
+import os
+
+# 5 funcion volumen del prisma
+
+m=float(os.sys.argv[1])
+n=float(os.sys.argv[2])
+s=libreria.volumen_prisma(m,n)
+print("El volumen del prisma es: ", s)
