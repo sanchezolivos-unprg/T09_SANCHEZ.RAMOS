3218d6f7d32230bdf24d913591e44c0b49c381da
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-20 08:34:45 -0500

    se agrego el python file app_4.py en funcion de la libreria.py

---
+++ sanchez/app_4.py
@@ -0,0 +1,9 @@
+import libreria
+import os
+
+# 4 funcion volumen del cilindro
+
+h=float(os.sys.argv[1])
+r=float(os.sys.argv[2])
+a=libreria.volumen_cilindro(h,r)
+print("El volumen del cilindro es: ", a)
