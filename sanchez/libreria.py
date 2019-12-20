1f191482b58aa49a679d4a8d5b5cea1dccdd2e1d
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-20 07:59:47 -0500

    se agrego la cuarta funcion a python file libreria.py al directory sanchez

--- sanchez/libreria.py
+++ sanchez/libreria.py
@@ -11,6 +11,13 @@ def fuerza (masa_kg,aceleracion):
     return resultado
 
 # funcion 3: calculo del area del triangulo
+
 def area_triangulo (base,haltura):
     calculo=((base*haltura)/2)
     return calculo
+
+# funcion 4: volumen del cilindro
+
+def volumen_cilindro (haltura,radio):
+    resultado=(3.14*(radio**2)*haltura)
+    return resultado
