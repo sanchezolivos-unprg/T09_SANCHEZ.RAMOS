9c779409d0c12c6956c7fad8af69e57b6fb0592c
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-20 08:50:00 -0500

    se agrego el python file app_13.py en funcion de la libreria.py

---
+++ sanchez/app_13.py
@@ -0,0 +1,9 @@
+import libreria
+import os
+
+# 13 funcion rapidez de un corredor
+
+d=float(os.sys.argv[1])
+t=float(os.sys.argv[2])
+f=libreria.rapidez(d,t)
+print("La rapidez del crredor es: ", f)
