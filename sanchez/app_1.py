1e6a41f24943d501899c56c9cad949e2352d60f8
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-20 08:28:02 -0500

    se agrego el python file app1.py en funcion de la libreria.py

---
+++ sanchez/app_1.py
@@ -0,0 +1,10 @@
+import libreria
+import os
+
+# 1 funcion del calculo de vista
+
+a=float(os.sys.argv[1])
+b=int(os.sys.argv[2])
+x=libreria.cal_vista(a,b)
+print("El calculo de la medicion de lentes es: ", x)
+
