b7cd93ba9f195d78fdf204d34f0152d5568e9aff
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-20 08:56:39 -0500

    se agrego el python file app_19.py en funcion de la libreria.py

---
+++ sanchez/app_19.py
@@ -0,0 +1,10 @@
+import libreria
+import os
+
+# 19 funcion multiplicacion
+
+x=float(os.sys.argv[1])
+y=float(os.sys.argv[2])
+z=float(os.sys.argv[3])
+f=libreria.multiplicar(x,y,z)
+print("El resultado de la multiplicacion es: ", f)
