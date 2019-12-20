8a5f4c0106a226702c1fc4bae2025fcd56873797
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-20 09:42:14 -0500

    se agrego el python file app13.py de mi compañero ramos_tito al directory ramos_tito

---
+++ ramos_tito/app13.py
@@ -0,0 +1,8 @@
+import libreria
+import os
+#ejercicio 13 llamar funcion: volumen_celindro
+
+ab=int(os.sys.argv[1])
+altura=int(os.sys.argv[2])
+volumen=libreria.volumen_celindro(ab,altura)
+print(volumen)
