e2cb001e1892c8564804506a78ee42c7e4d8a283
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-20 08:39:16 -0500

    se agrego el python file app_9.py en funcion de la libreria.py

---
+++ sanchez/app_9.py
@@ -0,0 +1,11 @@
+import libreria
+import os
+
+import math
+
+# 9 funcion area de un triangulo rectangulo
+
+a=float(os.sys.argv[1])
+b=float(os.sys.argv[2])
+p=libreria.area_tri_rectangulo(a,b)
+print("El area del triangulo rectangulo es: ", p)
