866aad3312fcb3ae693940f0a4b60f5a2f24c1ec
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-20 08:53:57 -0500

    se agrego el python file app_17.py en funcion de la libreria.py

---
+++ sanchez/app_17.py
@@ -0,0 +1,10 @@
+import libreria
+import os
+
+# 17 funcion area de un trapecio
+
+a=float(os.sys.argv[1])
+b=float(os.sys.argv[2])
+c=float(os.sys.argv[3])
+v=libreria.area_trapecio(a,b,c)
+print("El area del trapecio es de: ", v)
