af15f18a1639c937ce5937c9b7499ed912e85930
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-20 08:33:55 -0500

    se agrego el python file app_3.py en funcion de la libreria.py

---
+++ sanchez/app_3.py
@@ -0,0 +1,9 @@
+import libreria
+import os
+
+# 3 funcion area del triangulo
+
+ba=float(os.sys.argv[1])
+ha=float(os.sys.argv[2])
+z=libreria.area_triangulo(ba,ha)
+print("El area del triangulo es: ", z)
