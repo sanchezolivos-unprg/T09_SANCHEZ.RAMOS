fcf3fd414c0ea3db07e27d7b680ccca887598c90
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-20 08:51:44 -0500

    se agrego el python file app_15.py en funcion de la libreria.py

---
+++ sanchez/app_15.py
@@ -0,0 +1,9 @@
+import libreria
+import os
+
+# 15 funcion volumen del cono recto
+
+a=float(os.sys.argv[1])
+b=float(os.sys.argv[2])
+j=libreria.volumen_cono(a,b)
+print("El volumen del cono recto es: ", j)
