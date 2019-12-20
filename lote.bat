3fbe814882e61d13d8c0ae96b53e407b5a2e0771
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-20 09:08:13 -0500

    se agrego el lote.bat de las funciones con apps

---
+++ lote.bat
@@ -0,0 +1,79 @@
+python sanchez/app_1.py 4.0 20
+python sanchez/app_1.py 3.5 36
+python sanchez/app_1.py 2.5 10
+
+python sanchez/app_2.py 45.0 3.0
+python sanchez/app_2.py 105.5 5.4
+python sanchez/app_2.py 36.8 6.5
+
+python sanchez/app_3.py 5.0 8.5
+python sanchez/app_3.py 7.5 4.0
+python sanchez/app_3.py 8.8 25.0
+
+python sanchez/app_4.py 20.0 3.0
+python sanchez/app_4.py 15.7 4.8
+python sanchez/app_4.py 50.2 16.2
+
+python sanchez/app_5.py 6.8 8.5
+python sanchez/app_5.py 10.5 3.5
+python sanchez/app_5.py 21.0 5.4
+
+python sanchez/app_6.py 58.0 25 1.60
+python sanchez/app_6.py 64.4 30 1.78
+python sanchez/app_6.py 55.5 16 1.49
+
+python sanchez/app_7.py 12.0 15.0 10.5
+python sanchez/app_7.py 0.0 13.4 11.9
+python sanchez/app_7.py 14.4 16.5 18.0
+
+python sanchez/app_8.py 48.0 10.0
+python sanchez/app_8.py 21.5 8.4
+python sanchez/app_8.py 65.5 7.8
+
+python sanchez/app_9.py 3.0 4.0
+python sanchez/app_9.py 5.2 6.2
+python sanchez/app_9.py 12.0 8.6
+
+python sanchez/app_10.py 10.0 4.0
+python sanchez/app_10.py 12.5 5.5
+python sanchez/app_10.py 25.8 14.0
+
+python sanchez/app_11.py 12.0
+python sanchez/app_11.py 15.4
+python sanchez/app_11.py 36.5
+
+python sanchez/app_12.py 15 3.0
+python sanchez/app_12.py 21 4.5
+python sanchez/app_12.py 12 15.8
+
+python sanchez/app_13.py 100.0 11.0
+python sanchez/app_13.py 150.5 20.2
+python sanchez/app_13.py 200.0 40.4
+
+python sanchez/app_14.py 4.0 5.0 3.0
+python sanchez/app_14.py 20.8 15.5 9.0
+python sanchez/app_14.py 0.8 1.5 2.5
+
+python sanchez/app_15.py 5.0 3.0
+python sanchez/app_15.py 7.5 2.4
+python sanchez/app_15.py 15.0 1.5
+
+python sanchez/app_16.py 25 7.0
+python sanchez/app_16.py 27 6.8
+python sanchez/app_16.py 36 12.4
+
+python sanchez/app_17.py 12.0 20.0 5.0
+python sanchez/app_17.py 14.4 18.5 7.4
+python sanchez/app_17.py 16.6 38.7 15.0
+
+python sanchez/app_18.py 4.2 5.8 10.6 38.4
+python sanchez/app_18.py 27.3 15.6 14.5 120.0
+python sanchez/app_18.py 400.5 50.6 120.7 18.7
+
+python sanchez/app_19.py 10.2 15.8 16.8
+python sanchez/app_19.py 21.0 19.6 12.0
+python sanchez/app_19.py 14.8 150.6 14.5
+
+python sanchez/app_20.py 456411
+python sanchez/app_20.py las tortugas
+python sanchez/app_20.py 97451236
